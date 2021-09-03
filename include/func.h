1befa88f743a902fb04ea5a5c4bac6da506754d8
Selvavignesh <59078469+selva-vignesh@users.noreply.github.com>
2021-09-03 15:55:59 +0530

    Add files via upload

---
+++ include/func.h
@@ -0,0 +1,13 @@
+void module_func(int,int);
+void interest_mod(int, void (*func[10])());
+void mod1();
+void mod2();
+void mod3();
+void mod4();
+void mod5();
+void mod6();
+void mod7();
+void mod8();
+void mod9();
+void mod10();
+
